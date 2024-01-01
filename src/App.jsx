0f6f17327b0d0492064979e43ac6258ea9977a90
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-01-01 18:36:50 +0330

    Layout Complated (Header and Footer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import Layout from "./assets/components/template/layout";
+
 export default function App() {
+  const [filter, setFilter] = useState({
+    search: "",
+    currency: "usd",
+  });
   return (
     <div>
-      
+      <Layout filter={filter} setFilter={setFilter}>
+        <h1>Layout Complated</h1>
+      </Layout>
     </div>
-  )
+  );
 }
